e9c27f5772cbb3be1a1e5390c36ec4374e4f2173
Anton Kruglyakov <akrugs@mail.ru>
2023-03-25 03:53:22 +0500

    Задача 43

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
-//*/
+/*/
 void CheckNaturalNuber()
 {
 
@@ -32,4 +32,30 @@ void CheckNaturalNuber()
 CheckNaturalNuber();
 //*/
 
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+//*/
+void IntersectionPoint()
+{
+    Console.WriteLine("Введите параметры двух прямых для определения точки их пересечения на плоскости...");
 
+    Console.Write("Введите k1: ");
+    double k1 = double.Parse(Console.ReadLine()!);
+    Console.Write("Введите k2: ");
+    double k2 = double.Parse(Console.ReadLine()!);
+    Console.Write("Введите b1: ");
+    double b1 = double.Parse(Console.ReadLine()!);
+    Console.Write("Введите b2: ");
+    double b2 = double.Parse(Console.ReadLine()!);
+
+    if(k1 != k2) {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        Console.Write($"Прямые пересекаются в точке (x,y): ( {x} , {y} )");
+    } else {
+        Console.Write($"Прямые параллельны! Они не пересекаются на плоскости...");
+        }
+}
+
+IntersectionPoint();
+//*/
